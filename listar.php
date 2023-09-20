ac99efa25cab8fb955ab7d7487a9e3a9bdc8dec8
unknown <motaviny140@gmail.com>
2023-09-20 12:27:05 -0300

    Iniciando estilização

--- listar.php
+++ listar.php
@@ -15,6 +15,7 @@ use Kreait\Firebase\Factory;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de animais</title>
+    <link rel="stylesheet" href="./assets/css/estilos.css">
 </head>
 <body>
     <table>
